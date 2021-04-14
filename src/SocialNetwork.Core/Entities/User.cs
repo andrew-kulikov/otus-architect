a20ac823662ae160ab5629061f5e36f25619594c
Andrew <andrew.1.kulikov@gmail.com>
2021-04-14 22:37:26 +0300

    Add User and UserProfile entities

---
+++ src/SocialNetwork.Core/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork.Core.Entities
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime RegisteredAt { get; set; }
+    }
+}
---
+++ src/SocialNetwork.Core/Entities/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿namespace SocialNetwork.Core.Entities
+{
+    public class UserProfile
+    {
+        public long UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+    }
+}
