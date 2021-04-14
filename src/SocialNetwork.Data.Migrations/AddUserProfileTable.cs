abdd533aac7793d36739b71df0c3b4517a11a56c
Andrew <andrew.1.kulikov@gmail.com>
2021-04-14 22:08:40 +0300

    Add migration for UserProfile

---
+++ src/SocialNetwork.Data.Migrations/AddUserProfileTable.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace SocialNetwork.Data.Migrations
+{
+    [Migration(20210414205)]
+    public class AddUserProfileTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("UserProfile")
+                .WithColumn("UserId").AsInt64().PrimaryKey().ForeignKey("User", "Id")
+                .WithColumn("FirstName").AsString()
+                .WithColumn("LastName").AsString()
+                .WithColumn("Age").AsInt32()
+                .WithColumn("Interests").AsString(int.MaxValue)
+                .WithColumn("City").AsString();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("UserProfile");
+        }
+    }
+}
