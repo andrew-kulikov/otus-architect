134c33ca78625e1b71e09e7b5c41d199fe383f31
Andrew <andrew.1.kulikov@gmail.com>
2021-04-14 21:23:55 +0300

    Add migrations project with initial User table migration

---
+++ src/SocialNetwork.Data.Migrations/AddUserTable.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace SocialNetwork.Data.Migrations
+{
+    [Migration(202104142105)]
+    public class AddUserTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("User")
+                .WithColumn("Id").AsGuid().PrimaryKey().Identity()
+                .WithColumn("Username").AsString().Unique().NotNullable()
+                .WithColumn("Email").AsString().Unique().NotNullable()
+                .WithColumn("PasswordHash").AsString().NotNullable()
+                .WithColumn("RegisteredAt").AsDateTime().NotNullable()
+                .WithColumn("RegistrationCompleted").AsBoolean().NotNullable().WithDefaultValue(false);
+        }
+
+        public override void Down()
+        {
+            Delete.Table("User");
+        }
+    }
+}
