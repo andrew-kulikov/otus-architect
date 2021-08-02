c772c333c8f3e6b8870bdd19791ae65bee92ebcc
Andrew <andrew.1.kulikov@gmail.com>
2021-08-02 23:56:07 +0300

    Add styles to news feed cards

--- src/SocialNetwork.Infrastructure/Caching/RedisListCache.cs
+++ src/SocialNetwork.Infrastructure/Caching/RedisListCache.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 using StackExchange.Redis.Extensions.Core.Abstractions;
 
 namespace SocialNetwork.Infrastructure.Caching
@@ -8,16 +10,38 @@ namespace SocialNetwork.Infrastructure.Caching
     public class RedisListCache<T>: IListCache<T>
     {
         private readonly IRedisCacheClient _redisCacheClient;
+        private readonly ILogger<RedisListCache<T>> _logger;
 
-        public RedisListCache(IRedisCacheClient redisCacheClient)
+        public RedisListCache(IRedisCacheClient redisCacheClient, ILogger<RedisListCache<T>> logger)
         {
             _redisCacheClient = redisCacheClient;
+            _logger = logger;
         }
 
-        public async Task<List<T>> GetAsync(string key) => 
-            await _redisCacheClient.GetDbFromConfiguration().GetAsync<List<T>>(key);
+        public async Task<List<T>> GetAsync(string key)
+        {
+            try
+            {
+                return await _redisCacheClient.GetDbFromConfiguration().GetAsync<List<T>>(key);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Redis exception: {e.Message}");
 
-        public async Task SetAsync(string key, List<T> data) => 
-            await _redisCacheClient.GetDbFromConfiguration().AddAsync(key, data, DateTimeOffset.UtcNow.AddHours(6));
+                return new List<T>();
+            }
+        }
+
+        public async Task SetAsync(string key, List<T> data)
+        {
+            try
+            {
+                await _redisCacheClient.GetDbFromConfiguration().AddAsync(key, data, DateTimeOffset.UtcNow.AddHours(6));
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Redis exception: {e.Message}");
+            }
+        }
     }
 }
