6a80b0248119dec0d662faab73226350f76c03cd
Andrew <andrew.1.kulikov@gmail.com>
2021-09-09 00:14:05 +0300

    HW-6 Add empty chats page

---
+++ src/SocialNetwork.Web/ViewModels/ChatsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SocialNetwork.Core.Entities;
+
+namespace SocialNetwork.Web.ViewModels
+{
+    public class ChatsViewModel
+    {
+        public ICollection<Chat> ChatMembers { get; set; }
+    }
+}
