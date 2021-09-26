3f2304b2151502536f02afe5f64660f898e70d3c
Andrew <andrew.1.kulikov@gmail.com>
2021-09-26 12:24:23 +0300

    PROJ Move feed url to configuration

--- final/backend/FeedHistory.Service.Listener/Worker.cs
+++ final/backend/FeedHistory.Service.Listener/Worker.cs
@@ -6,6 +6,7 @@ using FeedHistory.Common;
 using FeedHistory.Service.Listener.Builders;
 using FeedHistory.Service.Listener.Storage;
 using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -18,12 +19,14 @@ namespace FeedHistory.Service.Listener
         private readonly BarsBuilder _barsBuilder;
         private readonly IBarsRepository _barsRepository;
         private readonly IDbInitializer _dbInitializer;
+        private readonly IConfiguration _configuration;
 
-        public Worker(ILogger<Worker> logger, IBarsRepository barsRepository, IDbInitializer dbInitializer)
+        public Worker(ILogger<Worker> logger, IBarsRepository barsRepository, IDbInitializer dbInitializer, IConfiguration configuration)
         {
             _logger = logger;
             _barsRepository = barsRepository;
             _dbInitializer = dbInitializer;
+            _configuration = configuration;
             _barsBuilder = new BarsBuilder(_barsRepository);
         }
 
@@ -32,7 +35,7 @@ namespace FeedHistory.Service.Listener
             await _dbInitializer.InitializeAsync();
 
             var connection = new HubConnectionBuilder()
-                .WithUrl("https://localhost:5001/feedHub")
+                .WithUrl(_configuration.GetValue<string>("Feed:Url"))
                 .WithAutomaticReconnect()
                 .AddJsonProtocol()
                 .Build();
