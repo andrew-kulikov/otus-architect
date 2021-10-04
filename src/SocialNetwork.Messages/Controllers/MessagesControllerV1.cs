22a5f3d10e7e85b705ab1d7406018bd51b06c01c
Andrew <andrew.1.kulikov@gmail.com>
2021-10-04 23:01:39 +0300

    HW-8 Add chat service template

---
+++ src/SocialNetwork.Messages/Controllers/MessagesControllerV1.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.Messages.Controllers
+{
+    [ApiController]
+    [Route("api/messages/v1")]
+    public class MessagesControllerV1 : ControllerBase
+    {
+    }
+}
